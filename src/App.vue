745a654fe0775dabc07608471ee753244c678f9e
Chris <chrispanetta123@gmail.com>
2019-10-16 13:14:28 +1100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<input type="file" id="csvdata" @change="loadTextFromFile" />
+		<button v-if="data != null" type="button" v-on:click="toggleTable">Show Table</button>
+		<!-- {{data}} -->
+		<TableDisplay v-if="showTable" v-bind:data="data"></TableDisplay>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableDisplay from "./components/TableDisplay.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	name: "app",
+	components: {
+		TableDisplay
+	},
+	data() {
+		return {
+			data: null,
+			showTable: false
+		};
+	},
+	methods: {
+		toggleTable() {
+			this.showTable = !this.showTable;
+		},
+		loadTextFromFile(ev) {
+			var files = ev.target.files || ev.dataTransfer.files;
+			if (!files.length) return;
+			this.createInput(files[0]);
+		},
+		createInput(file) {
+			var reader = new FileReader();
+			var vm = this;
+			reader.onload = e => {
+				vm.data = this.csvJSON(reader.result);
+			};
+			reader.readAsText(file);
+		},
+		csvJSON(csv) {
+			var lines = csv.split("\n");
+
+			var result = [];
+
+			var headers = lines[0].split(",");
+
+			for (var i = 1; i < lines.length; i++) {
+				var obj = {};
+				var currentline = lines[i].split(",");
+
+				for (var j = 0; j < headers.length; j++) {
+					obj[headers[j]] = currentline[j];
+				}
+				result.push(obj);
+			}
+			console.log(result);
+			return result; //JavaScript object
+			// return JSON.stringify(result); //JSON
+			// return JSON.parse(result);
+		}
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/TableDisplay.vue
@@ -0,0 +1,75 @@
+<template>
+	<div>
+		<h3>
+			filter by suburb
+			<input type="search" v-model="search" />
+		</h3>
+		<br />
+		<p>Displaying: {{displayMax}}</p>
+		<p>
+			Display all?
+			<input type="checkbox" @click="displayMax = data.length" />
+		</p>
+		<table>
+			<th @click="sort('SUBURB')">Suburb</th>
+			<th @click="sort('POSTCODE')">Postcode</th>
+			<th @click="sort('STATE')">State</th>
+			<th @click="sort('COUNTRY')">Country</th>
+			<th @click="sort('PO BOX')">Post box</th>
+			<th @click="sort('LONG')">Longitude</th>
+			<th @click="sort('LAT\r\r')">Latitude</th>
+			<tr v-for="object in filteredSearch.slice(0,displayMax)">
+				<td>{{object.SUBURB}}</td>
+				<td>{{object.POSTCODE}}</td>
+				<td>{{object.STATE}}</td>
+				<td>{{object.COUNTRY}}</td>
+				<td>{{object['PO BOX']}}</td>
+				<td>{{object.LONG}}</td>
+				<td>{{object['LAT\r\r']}}</td>
+			</tr>
+		</table>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DisplayTable",
+	props: ["data"],
+	data() {
+		return {
+			displayMax: 50,
+			search: "",
+			currentSort: "SUBURB",
+			currentSortDir: "ASC"
+		};
+	},
+	methods: {
+		sort: function(s) {
+			//if s == current sort, reverse
+			if (s === this.currentSort) {
+				this.currentSortDir =
+					this.currentSortDir === "asc" ? "desc" : "asc";
+			}
+			this.currentSort = s;
+		}
+	},
+	computed: {
+		filteredSearch: function() {
+			return this.sortedObject.filter(object => {
+				return object.SUBURB.toLowerCase().match(this.search);
+			});
+		},
+		sortedObject: function() {
+			return this.data.sort((a, b) => {
+				let modifier = 1;
+				if (this.currentSortDir === "desc") modifier = -1;
+				if (a[this.currentSort] < b[this.currentSort])
+					return -1 * modifier;
+				if (a[this.currentSort] > b[this.currentSort])
+					return 1 * modifier;
+				return 0;
+			});
+		}
+	}
+};
+</script>
